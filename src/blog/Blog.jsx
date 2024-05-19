00c3c99f97426ec4180c585c2769a61a0e5a1f3d
bjrfx <phanikiran1234@gmail.com>
2024-05-19 08:34:29 -0400

    Build

--- src/blog/Blog.jsx
+++ src/blog/Blog.jsx
@@ -8,6 +8,7 @@ const Blog = () => {
     <div className='sub_page'>
         <CommonHeader />
         <h1 style = {{ textAlign: 'center'}}>Blog Posts Here</h1>
+        <p style = {{ textAlign: 'center'}}>Post 1</p>
         <Info />
         <Footer />
     </div>
