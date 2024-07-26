261895b0d72c9ae9f1431ac85cd6366e9cb21abc
bjrfx <phanikiran1234@gmail.com>
2024-07-26 00:21:00 -0400

    fix test-2
    #8848d87
    page-not-found back button fix

--- src/pages/pageNotFound/PageNotFound.jsx
+++ src/pages/pageNotFound/PageNotFound.jsx
@@ -1,26 +1,18 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button'
-import { Link, useNavigate } from 'react-router-dom'
-const PageNotFound = () => {
-    const navigate = useNavigate();
-    const handleClick = (to) => (e) => {
-        e.preventDefault();
-        navigate(to);
-        window.scrollTo(0, 0);
-    };
-    return (
-        <div>
-            <h1 style={{ textAlign: "center", fontWeight: "bold", marginTop: "23rem" }}>Page Not Found</h1>
-            <div className="d-flex justify-content-center">
-                <Link onClick={handleClick('/')} to="/">
-                    <Button variant="outline-dark">
-                        Go Back
-                    </Button>
-                </Link>
-            </div>
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 
-        </div>
-    )
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", fontWeight: "bold", marginTop: "23rem" }}>Page Not Found</h1>
+      <div className="d-flex justify-content-center">
+        <Button variant="outline-dark">
+        <a href="https://www.bdotsoftware.com" style={{textDecoration: 'none', color: "inherit"}}>Go Back</a>
+        </Button>
+      </div>
+    </div>
+  )
 }
 
-export default PageNotFound
+export default PageNotFound;
