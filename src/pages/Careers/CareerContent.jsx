d797a92543bc9c283b939548873ef7acdfd6703e
bjrfx <phanikiran1234@gmail.com>
2024-07-26 01:24:49 -0400

    career for update

--- src/pages/Careers/CareerContent.jsx
+++ src/pages/Careers/CareerContent.jsx
@@ -30,7 +30,7 @@ const CareerContent = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:5000/apply', formData)
+    axios.post('http://localhost:5000/careers', formData)
       .then((response) => {
         alert('Application submitted successfully');
         setShow(false);
